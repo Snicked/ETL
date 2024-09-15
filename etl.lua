65c533ebb9f55e07cba2a6168fd1a7f5e146d436
Snicked <rox_x0r@yahoo.de>
2024-09-15 09:16:33 +0200

    Added Slash Command for Reset

--- etl.lua
+++ etl.lua
@@ -7,6 +7,22 @@ local update_display
 
 local last_update = 0
 
+SLASH_ETL1= "/etl"
+SlashCmdList["ETL"] = function(message)
+	local cmd = { }
+	for c in string.gfind(message, "[^ ]+") do
+		table.insert(cmd, string.lower(c))
+	end
+
+	if cmd[1] == "clear" then
+		ETL_xp_gains = {}
+		ETL_frame_coordinates = { x = 0, y = 0 }
+	else
+		DEFAULT_CHAT_FRAME:AddMessage("etl usage:")
+		DEFAULT_CHAT_FRAME:AddMessage("/etl clear - clear saved data")
+	end
+end
+
 function ETL_on_xp_gain()
 	local _, _, xp_part = strfind(arg1, '(%d+)')
 	local gained_xp = tonumber(xp_part)
@@ -31,29 +47,27 @@ function ETL_on_update()
 			tremove(ETL_xp_gains)
 		end
 		local xp_per_hour = 0
-		local allXp = 0
 		local counter = 0
 		local average = 0
 		for _, xp_gain in ipairs(ETL_xp_gains) do
 			xp_per_hour = xp_per_hour + xp_gain.xp
-			allXp = allXp + xp_gain.xp
 			counter = counter + 1
 		end
 		if counter > 0 then
-			average = allXp / counter
+			average = xp_per_hour / counter
 		end
-    if ETL_xp_gains[1] then
-      local offset = (GetTime() - ETL_xp_gains[1].t)
-      xp_per_hour = (xp_per_hour / offset) * 3600
-    end
-    local remainingXp=(UnitXPMax('player') - UnitXP('player'));
-		local etl =  remainingXp / xp_per_hour *  60
+		if ETL_xp_gains[1] then
+			local offset = GetTime() - ETL_xp_gains[1].t
+			xp_per_hour = (xp_per_hour / offset) * 3600
+		end
+		local remaining_xp = UnitXPMax('player') - UnitXP('player')
+		local etl =  remaining_xp / xp_per_hour *  60
 		local etl_hours = math.floor(etl / 60)
 		local etl_minutes = math.ceil(etl - 60 * etl_hours)
 		local kills =	'N/A'
-    if average > 0 then 
-      kills = math.ceil(remainingXp / average )
-    end
+		if average > 0 then 
+			kills = math.ceil(remaining_xp / average )
+		end
 		update_display(xp_per_hour, etl_hours, etl_minutes,kills)
 	end
 end
@@ -78,3 +92,4 @@ end
 
 
 
+q
